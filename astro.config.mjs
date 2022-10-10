0d95b7dd9d09cff8645c009385501440ab35f84e
Rafael Vidal <vidal@live.com>
2022-10-10 22:00:35 +0800

    astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import vue from "@astrojs/vue"
+import vue from "@astrojs/vue";
+import { astroImageTools } from "astro-imagetools";
 
-import { astroImageTools } from "astro-imagetools"
+// https://astro.build/config
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), vue(), astroImageTools],
-})
+  integrations: [tailwind(), vue(), astroImageTools, compress()]
+});
