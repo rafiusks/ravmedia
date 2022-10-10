1aa70a8d35c9008fd392e85323f5c66e5b83c645
Rafael Vidal <vidal@live.com>
2022-10-10 21:51:07 +0800

    basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
-import vue from "@astrojs/vue";
+import vue from "@astrojs/vue"
+
+import { astroImageTools } from "astro-imagetools"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
-});
+	integrations: [tailwind(), vue(), astroImageTools],
+})
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+	<footer class="container mx-auto py-10 text-right text-sm">
+		Built with: <a href="//tailwindcss.com">tailwindcss</a> ,
+		<a href="//reactjs.org">React</a>, and <a href="//vue.org">Vue</a>. What?
+		React and Vue, how? <a href="//astro.build" target="_blank">Astro</a>
+	</footer>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style></style>
