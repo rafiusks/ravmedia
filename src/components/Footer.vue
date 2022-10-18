ebbc7c7cd0773036d96f2f3249bd5b91c5cd46a9
Rafael Vidal <vidal@live.com>
2022-10-18 23:18:25 +0800

    new format

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 	<footer class="container mx-auto py-10 text-right text-sm">
 		Built with: <a href="//tailwindcss.com">tailwindcss</a> ,
 		<a href="//reactjs.org">React</a>, and <a href="//vue.org">Vue</a>. What?
-		React and Vue, how? <a href="//astro.build" target="_blank">Astro</a>
+		React and Vue, how? <a href="//astro.build" target="_blank">-> Astro</a>
 	</footer>
 </template>
 
