1d149177e8ca61a0f335b57a415d6773c706cd55
Rafael Vidal <vidal@live.com>
2024-11-05 20:16:10 +0800

    upgrade to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import vue from "@astrojs/vue";
-import { astroImageTools } from "astro-imagetools";
-
-// https://astro.build/config
-import compress from "astro-compress";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), astroImageTools, compress()]
+	integrations: [tailwind(), react()],
 });
